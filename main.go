4043011dbe0582d0ccd12054903fd9b50860cbba
Yovanny Diaz <thebigyovadiaz@gmail.com>
2024-02-20 10:23:00 -0300

    Refactor websocket handler

--- main.go
+++ main.go
@@ -2,39 +2,51 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// received and show message
+		log.Printf("Received message: %s\n", message)
+
+		msgReceived := fmt.Sprintf("Message received: %s\n", message)
+
+		// Send the message back to the client
+		err = conn.WriteMessage(websocket.TextMessage, []byte(msgReceived))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
-	_ = http.ListenAndServe(":2323", nil)
+	http.HandleFunc("/ws-message", websocketHandler)
+	log.Fatal(http.ListenAndServe(":2323", nil))
 }
